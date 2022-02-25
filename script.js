5b3932123b232249b6d8a90a87e940f990c7a09c
robert hudek <hudektech@gmail.com>
2022-02-25 11:45:06 +0800

    add filter

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let picNum = 1;
 let picMin = 1;
 let picMax = 3;
 
+let filterNum = 0;
+
 function shift(n) {
   let sel;
   let oldPic;
@@ -42,4 +44,41 @@ function shift(n) {
   friendName.innerText = newPic.alt;
 
   console.log("After click picture will be " + picNum);
+}
+
+function filter(n) {
+  let r = document.querySelector(":root");
+  let rs = getComputedStyle(r);
+
+  let filters = [
+    { name: "No Filter", var: "" },
+    { name: "Blur", var: "--filter-blur" },
+    { name: "Contrast", var: "--filter-contrast" },
+    { name: "Grayscale", var: "--filter-grayscale" },
+    { name: "Rotate Hue 180", var: "--filter-huerotate" },
+    { name: "Invert Color", var: "--filter-invert" },
+    { name: "Saturate Color", var: "--filter-saturate" },
+    { name: "Sepia", var: "--filter-sepia" },
+    { name: "Green Shadow", var: "--filter-shadow" },
+  ];
+
+  let filterMin = 0;
+  let filterMax = filters.length - 1;
+
+  filterNum = filterNum + n;
+
+  if (filterNum > filterMax) {
+    filterNum = filterMin;
+  }
+  if (filterNum < filterMin) {
+    filterNum = filterMax;
+  }
+
+  r.style.setProperty("--filter", rs.getPropertyValue(filters[filterNum].var));
+
+  document.getElementById("filter").innerText = filters[filterNum].name;
+  // r.style.setProperty("--filter", "--filter-sepia");
+  // r.style.setProperty("--filter", rs.getPropertyValue(theme.background));
+  // Remove pic-on class for picNum
+  // 1) get the pic
 }

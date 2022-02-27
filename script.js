1d67fc4d3469c62925c137475b310f33de01daed
robert hudek <hudektech@gmail.com>
2022-02-27 14:24:28 +0800

    change to travel

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // globals
 let picNum = 1;
 let picMin = 1;
-let picMax = 3;
-
-let filterNum = 0;
+let picMax = document.querySelectorAll("img").length;
 
 function shift(n) {
   let sel;
   let oldPic;
   let newPic;
-  let friendName;
+  let picName;
 
   // Remove pic-on class for picNum
   // 1) get the old pic
@@ -37,47 +35,13 @@ function shift(n) {
   // 2) add the class 'pic-on'
   newPic.classList.add("pic-on");
 
-  // 3) update new friend name
-  // 3a) get h2
+  // 3) update new pic name
+  // 3a) get picName element
   // 3b) update innerText
-  friendName = document.getElementById("friend-name");
-  friendName.innerText = newPic.alt;
+  picName = document.getElementById("pic-name");
+  picName.innerText = newPic.alt;
 
   console.log("After click picture will be " + picNum);
 }
 
-function filter(n) {
-  let r = document.querySelector(":root");
-  let rs = getComputedStyle(r);
-
-  let filters = [
-    { name: "No Filter", var: "" },
-    // { name: "Blur", var: "--filter-blur" },
-    { name: "Contrast", var: "--filter-contrast" },
-    { name: "Grayscale", var: "--filter-grayscale" },
-    { name: "Rotate Hue 180", var: "--filter-huerotate" },
-    { name: "Invert Color", var: "--filter-invert" },
-    { name: "Saturate Color", var: "--filter-saturate" },
-    { name: "Sepia", var: "--filter-sepia" },
-    // { name: "Green Shadow", var: "--filter-shadow" },
-  ];
-
-  let filterMin = 0;
-  let filterMax = filters.length - 1;
-
-  // Get the selected filter
-  filterNum = filterNum + n;
-
-  if (filterNum > filterMax) {
-    filterNum = filterMin;
-  }
-  if (filterNum < filterMin) {
-    filterNum = filterMax;
-  }
-
-  // Update the css variable for this filter
-  r.style.setProperty("--filter", rs.getPropertyValue(filters[filterNum].var));
-
-  // Update the name of this filter
-  document.getElementById("filter").innerText = filters[filterNum].name;
-}
+document.getElementById("pic-name").innerText = document.getElementById("pic-1").alt;
